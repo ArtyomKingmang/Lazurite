3bc95aae9f6f23d3bc8a61ff2b0a0873051acfcf
ArtyomKingmang <kingmangapp@gmail.com>
2023-08-31 22:35:30 +0300

    Unit test module

---
+++ src/main/java/com/kingmang/lazurite/modules/unit/unit.java
@@ -0,0 +1,17 @@
+package com.kingmang.lazurite.modules.unit;
+import com.kingmang.lazurite.modules.Module;
+
+
+public final class unit implements Module {
+
+    public static void initConstants() {
+    }
+
+    @Override
+    public void init() {
+        initConstants();
+        //unit test
+    }
+    
+
+}
