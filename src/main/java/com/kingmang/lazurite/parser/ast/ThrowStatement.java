3f03fe84533d62481962b487b8d3524c66efa7e3
ArtyomKingmang <kingmangapp@gmail.com>
2023-09-02 15:30:54 +0300

    Add new module - Colors

---
+++ src/main/java/com/kingmang/lazurite/parser/ast/ThrowStatement.java
@@ -0,0 +1,51 @@
+package com.kingmang.lazurite.parser.ast;
+
+import com.kingmang.lazurite.lib._OperExeption;
+
+public class ThrowStatement implements Statement{
+
+    private String type;
+    private Expression expr;
+
+    public ThrowStatement(String type, Expression expr) {
+        this.type = type;
+        this.expr = expr;
+    }
+
+
+    @Override
+    public void execute() {
+        throw new _OperExeption(type, expr.eval().toString());
+    }
+
+    @Override
+    public String toString() {
+        return "throw " + type + " " + expr;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Expression getExpr() {
+        return expr;
+    }
+
+    public void setExpr(Expression expr) {
+        this.expr = expr;
+    }
+
+    @Override
+    public void accept(Visitor visitor) {
+
+    }
+
+    @Override
+    public <R, T> R accept(ResultVisitor<R, T> visitor, T input) {
+        return null;
+    }
+}
