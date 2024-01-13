ae067f81c2b4caf973bd35b55e1f3a4c0bd1a731
ArtyomKingmang <kingmangapp@gmail.com>
2024-01-13 18:53:36 +0300

    new function in system lib - getProperty

--- src/com/kingmang/lazurite/libraries/system/system.java
+++ src/com/kingmang/lazurite/libraries/system/system.java
@@ -1,6 +1,7 @@
 package com.kingmang.lazurite.libraries.system;
 
 import com.kingmang.lazurite.core.Arguments;
+import com.kingmang.lazurite.exceptions.LZRException;
 import com.kingmang.lazurite.libraries.Library;
 import com.kingmang.lazurite.runtime.*;
 import com.kingmang.lazurite.runtime.LZR.LZRMap;
@@ -12,16 +13,19 @@ import static com.kingmang.lazurite.runner.Exe.VERSION;
 
 public class system implements Library {
 
-    public void initConstant(){
+    public void init (){
 
         LZRMap values = new LZRMap(5);
-        LZRMap system = new LZRMap(6);
+        LZRMap system = new LZRMap(7);
+
+        //variables
         values.set("LZRVersion", new LZRString(VERSION()));
         values.set("JVMVersion", new LZRString(java.lang.System.getProperty("java.vm.version")));
         values.set("name", new LZRString(java.lang.System.getProperty("os.name")));
         values.set("arch", new LZRString(java.lang.System.getProperty("os.arch")));
         values.set("version", new LZRString(java.lang.System.getProperty("os.version")));
 
+        //functions
         system.set("currentTimeMillis", (Value...args) ->{
             Arguments.check(0, args.length);
             return LZRNumber.of(System.currentTimeMillis());
@@ -40,27 +44,9 @@ public class system implements Library {
             return LZRNumber.MINUS_ONE;
         });
 
-
-        LZRMap sep = new LZRMap(5);
-        sep.set("file", new LZRString(java.lang.System.getProperty("file.separator")));
-        sep.set("path", new LZRString(java.lang.System.getProperty("path.separator")));
-        sep.set("line", new LZRString(java.lang.System.getProperty("line.separator")));
-
-        LZRMap user = new LZRMap(5);
-        user.set("dir", new LZRString(java.lang.System.getProperty("user.dir")));
-        user.set("name", new LZRString(java.lang.System.getProperty("user.name")));
-
+        system.set("getProperty", (Value...args) -> new LZRString(System.getProperty(args[0].asString())));
         system.set("os", values);
-        system.set("os", values);
-        system.set("separator", sep);
-        system.set("user", user);
-
         Variables.define("system",system);
-
-    }
-
-    public void init (){
-        initConstant();
 
     }
 
