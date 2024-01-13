c017ec37edd33c8f7c916a97b29b0cfd6d0389bf
ArtyomKingmang <kingmangapp@gmail.com>
2024-01-13 18:43:05 +0300

    system lib update

--- src/com/kingmang/lazurite/libraries/system/system.java
+++ src/com/kingmang/lazurite/libraries/system/system.java
@@ -15,12 +15,22 @@ public class system implements Library {
     public void initConstant(){
 
         LZRMap values = new LZRMap(5);
+        LZRMap system = new LZRMap(6);
         values.set("LZRVersion", new LZRString(VERSION()));
         values.set("JVMVersion", new LZRString(java.lang.System.getProperty("java.vm.version")));
         values.set("name", new LZRString(java.lang.System.getProperty("os.name")));
         values.set("arch", new LZRString(java.lang.System.getProperty("os.arch")));
         values.set("version", new LZRString(java.lang.System.getProperty("os.version")));
-        values.set("exit", (Value... args) -> {
+
+        system.set("currentTimeMillis", (Value...args) ->{
+            Arguments.check(0, args.length);
+            return LZRNumber.of(System.currentTimeMillis());
+        });
+        system.set("nanoTime", (Value...args) ->{
+            Arguments.check(0, args.length);
+            return LZRNumber.of(System.nanoTime());
+        });
+        system.set("exit", (Value... args) -> {
             Arguments.check(1, args.length);
             try {
                 java.lang.System.exit((int) args[0].asNumber());
@@ -29,6 +39,8 @@ public class system implements Library {
             }
             return LZRNumber.MINUS_ONE;
         });
+
+
         LZRMap sep = new LZRMap(5);
         sep.set("file", new LZRString(java.lang.System.getProperty("file.separator")));
         sep.set("path", new LZRString(java.lang.System.getProperty("path.separator")));
@@ -38,9 +50,12 @@ public class system implements Library {
         user.set("dir", new LZRString(java.lang.System.getProperty("user.dir")));
         user.set("name", new LZRString(java.lang.System.getProperty("user.name")));
 
-        Variables.define("os", values);
-        Variables.define("separator", sep);
-        Variables.define("user", user);
+        system.set("os", values);
+        system.set("os", values);
+        system.set("separator", sep);
+        system.set("user", user);
+
+        Variables.define("system",system);
 
     }
 
