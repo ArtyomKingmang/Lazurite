4066b66163ebbe8549caee442e36640618660048
monsler <galileoru22@gmail.com>
2024-01-19 15:21:22 +0000

    Add exec method

--- src/com/kingmang/lazurite/libraries/system/system.java
+++ src/com/kingmang/lazurite/libraries/system/system.java
@@ -38,6 +38,7 @@ public class system implements Library {
             }
             return LZRNumber.MINUS_ONE;
         });
+
         system.set("getScreenResolution", new Function() {
             @Override
             public Value execute(Value... args) {
@@ -49,6 +50,21 @@ public class system implements Library {
                 return map;
             }
         });
+
+        system.set("exec", new Function() {
+            @Override
+            public Value execute(Value... args) {
+                Arguments.check(1, args.length);
+                try{
+                    Runtime.getRuntime().exec(args[0].asString());
+                    return LZRNumber.ZERO;
+                }catch (Exception ex){
+                    ex.printStackTrace();
+                }
+                return null;
+            }
+        });
+
         system.set("getProperty", (Value...args) -> {
             if(Objects.equals(args[0].asString(), "lzr.version")){
                 return new LZRString(VERSION());
