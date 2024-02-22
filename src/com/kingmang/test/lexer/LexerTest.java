628a23dc41f226292f77f1fb28ca2ce5b29c1d35
ArtyomKingmang <kingmangapp@gmail.com>
2024-02-22 17:19:56 +0300

    new tests for lexer

--- src/com/kingmang/test/lexer/LexerTest.java
+++ src/com/kingmang/test/lexer/LexerTest.java
@@ -10,6 +10,7 @@ import java.util.List;
 import static com.kingmang.lazurite.parser.parse.TokenType.*;
 import static com.kingmang.test.lexer.Helper.assertTokens;
 import static com.kingmang.test.lexer.Helper.list;
+import static junit.framework.TestCase.assertEquals;
 
 public class LexerTest {
 
@@ -23,6 +24,21 @@ public class LexerTest {
         assertTokens(expList, result);
     }
 
+    @Test
+    public void testOperators() {
+        String input = "=+-*/%<>!&|";
+        List<Token> expList = list(EQ, PLUS, MINUS, STAR, SLASH, PERCENT, LT, GT, EXCL, AMP, BAR);
+        List<Token> result = LexerImplementation.tokenize(input);
+        assertTokens(expList, result);
+    }
 
+    @Test
+    public void testString() {
+        String input = "\"1\\\"2\"";
+        List<Token> expList = list(TEXT);
+        List<Token> result = LexerImplementation.tokenize(input);
+        assertTokens(expList, result);
+        assertEquals("1\"2", result.get(0).getText());
+    }
     
 }
