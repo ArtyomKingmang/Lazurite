55b27ab6c8a2eaab22fa1d049248b734a60035fa
kingmang <kingmangapp@gmail.com>
2024-06-30 17:52:13 +0300

    preprocessor rewritten in Kotlin

--- src/main/java/com/kingmang/lazurite/parser/preprocessor/Preprocessor.kt
+++ src/main/java/com/kingmang/lazurite/parser/preprocessor/Preprocessor.kt
@@ -1,80 +1,79 @@
-package com.kingmang.lazurite.parser.preprocessor;
+package com.kingmang.lazurite.parser.preprocessor
 
-import com.kingmang.lazurite.runner.RunnerInfo;
-import com.kingmang.lazurite.utils.Handler;
+import com.kingmang.lazurite.runner.RunnerInfo
+import com.kingmang.lazurite.utils.Handler
+import java.util.*
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+object Preprocessor {
+    @JvmStatic
+    fun preprocess(code: String): String {
+        var code = code
+        var lines = Arrays.asList(*code.split("\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray())
+        val macros: MutableMap<String, String> = HashMap()
 
-public class Preprocessor {
-    public static String preprocess(String code) {
-        List<String> lines = Arrays.asList(code.split("\n"));
-        Map<String, String> macros = new HashMap<>();
+        macros["Lazurite"] = "\"" + RunnerInfo.getVersion() + "\""
 
-        macros.put("Lazurite", "\"" + RunnerInfo.getVersion() + "\"");
+        val codeBuilder = StringBuilder(code)
+        for (line in lines) {
+            val parts = Arrays.asList(*line.split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray())
+            if (parts.isEmpty()) continue
 
-        StringBuilder codeBuilder = new StringBuilder(code);
-        for (String line : lines) {
-            List<String> parts = Arrays.asList(line.split(" "));
-            if (parts.isEmpty()) continue;
-
-            String command = parts.get(0);
-            switch (command) {
-                case "#define":
-                    String id = parts.get(1);
-                    String replacement = String.join(" ", parts.subList(2, parts.size()));
+            val command = parts[0]
+            when (command) {
+                "#define" -> {
+                    val id = parts[1]
+                    val replacement = java.lang.String.join(" ", parts.subList(2, parts.size))
                     if (id.contains("(")) {
-                        codeBuilder.insert(0, "\n" + "func " + id + " return " + replacement + "\n");
-                        continue;
+                        codeBuilder.insert(0, "\nfunc $id return $replacement\n")
+                        continue
                     }
-                    macros.put(id, replacement);
-                    break;
+                    macros[id] = replacement
+                }
 
-                case "#ifdef":
-                    String idIfdef = parts.get(1);
-                    String replacementIfdef = String.join(" ", parts.subList(2, parts.size()));
-                    if (macros.containsKey(idIfdef)) Handler.handle(replacementIfdef, "<preprocessor>", true, false);
-                    break;
+                "#ifdef" -> {
+                    val idIfdef = parts[1]
+                    val replacementIfdef = java.lang.String.join(" ", parts.subList(2, parts.size))
+                    if (macros.containsKey(idIfdef)) Handler.handle(replacementIfdef, "<preprocessor>", true, false)
+                }
 
-                case "#ifndef":
-                    String idIfndef = parts.get(1);
-                    String replacementIfndef = String.join(" ", parts.subList(2, parts.size()));
-                    if (!macros.containsKey(idIfndef)) Handler.handle(replacementIfndef, "<preprocessor>", true, false);
-                    break;
+                "#ifndef" -> {
+                    val idIfndef = parts[1]
+                    val replacementIfndef = java.lang.String.join(" ", parts.subList(2, parts.size))
+                    if (!macros.containsKey(idIfndef)) Handler.handle(replacementIfndef, "<preprocessor>", true, false)
+                }
 
-                default:
-                    break;
+                else -> {}
             }
         }
-        code = codeBuilder.toString();
+        code = codeBuilder.toString()
 
-        lines = Arrays.asList(code.split("\n"));
-        StringBuilder result = new StringBuilder();
+        lines = Arrays.asList(*code.split("\n".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray())
+        var result = StringBuilder()
 
-        for (String line : lines) {
-            String buffer = line;
-            List<String> parts = Arrays.asList(line.split(" "));
-            if (parts.get(0).equals("#undef")) {
-                String idUndef = parts.get(1);
-                macros.remove(idUndef);
-                continue;
+        for (line in lines) {
+            var buffer = line
+            val parts = Arrays.asList(*line.split(" ".toRegex()).dropLastWhile { it.isEmpty() }.toTypedArray())
+            if (parts[0] == "#undef") {
+                val idUndef = parts[1]
+                macros.remove(idUndef)
+                continue
             }
 
-            for (Map.Entry<String, String> entry : macros.entrySet()) {
-                buffer = buffer.replaceAll(";", " ;");
-                buffer = buffer.replaceAll(String.format("(?<!\")(%s)(?!\")", entry.getKey()), entry.getValue()) + "\n";
+            for ((key, value) in macros) {
+                buffer = buffer.replace(";".toRegex(), " ;")
+                buffer = buffer.replace(String.format("(?<!\")(%s)(?!\")", key).toRegex(), value) + "\n"
             }
-            result.append(buffer);
+            result.append(buffer)
         }
 
-        result = new StringBuilder(result.toString()
+        result = StringBuilder(
+            result.toString()
                 .replace("#define", "# PROCESSED DEFINE MACROS")
                 .replace("#ifdef", "# PROCESSED IFDEF MACROS")
                 .replace("#ifndef", "# PROCESSED IFNDEF MACRO")
-                .replace("#undef", "# PROCESSED UNDEF MACRO"));
+                .replace("#undef", "# PROCESSED UNDEF MACRO")
+        )
 
-        return result.toString();
+        return result.toString()
     }
 }
