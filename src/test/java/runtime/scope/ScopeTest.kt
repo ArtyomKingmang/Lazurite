b46625ab637eeec98383c34b1dc54bc969a23aa7
RadiationX <radiationx@yandex.ru>
2024-07-10 13:17:43 +0500

    refactor: add tests  for Scopes, Libraries, Variables

---
+++ src/test/java/runtime/scope/ScopeTest.kt
@@ -0,0 +1,66 @@
+package runtime.scope
+
+import com.kingmang.lazurite.runtime.scope.Scope
+import com.kingmang.lazurite.runtime.scope.findOrRoot
+import org.junit.Test
+import kotlin.test.assertEquals
+
+class ScopeTest {
+
+    companion object {
+        private const val DATA = "root_data"
+    }
+
+    @Test
+    fun `find in empty root Expect root and notfound`() {
+        val root = Scope(null, setOf<String>())
+        val result = root.findOrRoot { it.data.contains(DATA) }
+        assertEquals(root, result.scope)
+        assertEquals(false, result.isFound)
+    }
+
+    @Test
+    fun `find in filled root Expect root and found`() {
+        val root = Scope(null, setOf(DATA))
+        val result = root.findOrRoot { it.data.contains(DATA) }
+        assertEquals(root, result.scope)
+        assertEquals(true, result.isFound)
+    }
+
+    @Test
+    fun `find in empty root and empty child Expect child and notfound`() {
+        val root = Scope(null, setOf<String>())
+        val child = Scope(root, setOf())
+        val result = child.findOrRoot { it.data.contains(DATA) }
+        assertEquals(child, result.scope)
+        assertEquals(false, result.isFound)
+    }
+
+    @Test
+    fun `find in empty root and filled child Expect child and found`() {
+        val root = Scope(null, setOf<String>())
+        val child = Scope(root, setOf(DATA))
+        val result = child.findOrRoot { it.data.contains(DATA) }
+        assertEquals(child, result.scope)
+        assertEquals(true, result.isFound)
+    }
+
+    @Test
+    fun `find in filled root and empty child Expect root and found`() {
+        val root = Scope(null, setOf(DATA))
+        val child = Scope(root, setOf())
+        val result = child.findOrRoot { it.data.contains(DATA) }
+        assertEquals(root, result.scope)
+        assertEquals(true, result.isFound)
+    }
+
+    @Test
+    fun `find in filled root and filled child Expect child and found`() {
+        val root = Scope(null, setOf(DATA))
+        val child = Scope(root, setOf(DATA))
+        val result = child.findOrRoot { it.data.contains(DATA) }
+        assertEquals(child, result.scope)
+        assertEquals(true, result.isFound)
+    }
+
+}
