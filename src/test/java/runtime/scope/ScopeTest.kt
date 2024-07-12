a04f2a3b3053b169ece5979e9bb101d6adb20338
RadiationX <radiationx@yandex.ru>
2024-07-12 14:24:52 +0500

    fix: find scope naming

--- src/test/java/runtime/scope/ScopeTest.kt
+++ src/test/java/runtime/scope/ScopeTest.kt
@@ -1,7 +1,7 @@
 package runtime.scope
 
 import com.kingmang.lazurite.runtime.scope.Scope
-import com.kingmang.lazurite.runtime.scope.findOrRoot
+import com.kingmang.lazurite.runtime.scope.findOrCurrent
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -14,7 +14,7 @@ class ScopeTest {
     @Test
     fun `find in empty root Expect root and notfound`() {
         val root = Scope(null, setOf<String>())
-        val result = root.findOrRoot { it.data.contains(DATA) }
+        val result = root.findOrCurrent { it.data.contains(DATA) }
         assertEquals(root, result.scope)
         assertEquals(false, result.isFound)
     }
@@ -22,7 +22,7 @@ class ScopeTest {
     @Test
     fun `find in filled root Expect root and found`() {
         val root = Scope(null, setOf(DATA))
-        val result = root.findOrRoot { it.data.contains(DATA) }
+        val result = root.findOrCurrent { it.data.contains(DATA) }
         assertEquals(root, result.scope)
         assertEquals(true, result.isFound)
     }
@@ -31,7 +31,7 @@ class ScopeTest {
     fun `find in empty root and empty child Expect child and notfound`() {
         val root = Scope(null, setOf<String>())
         val child = Scope(root, setOf())
-        val result = child.findOrRoot { it.data.contains(DATA) }
+        val result = child.findOrCurrent { it.data.contains(DATA) }
         assertEquals(child, result.scope)
         assertEquals(false, result.isFound)
     }
@@ -40,7 +40,7 @@ class ScopeTest {
     fun `find in empty root and filled child Expect child and found`() {
         val root = Scope(null, setOf<String>())
         val child = Scope(root, setOf(DATA))
-        val result = child.findOrRoot { it.data.contains(DATA) }
+        val result = child.findOrCurrent { it.data.contains(DATA) }
         assertEquals(child, result.scope)
         assertEquals(true, result.isFound)
     }
@@ -49,7 +49,7 @@ class ScopeTest {
     fun `find in filled root and empty child Expect root and found`() {
         val root = Scope(null, setOf(DATA))
         val child = Scope(root, setOf())
-        val result = child.findOrRoot { it.data.contains(DATA) }
+        val result = child.findOrCurrent { it.data.contains(DATA) }
         assertEquals(root, result.scope)
         assertEquals(true, result.isFound)
     }
@@ -58,7 +58,7 @@ class ScopeTest {
     fun `find in filled root and filled child Expect child and found`() {
         val root = Scope(null, setOf(DATA))
         val child = Scope(root, setOf(DATA))
-        val result = child.findOrRoot { it.data.contains(DATA) }
+        val result = child.findOrCurrent { it.data.contains(DATA) }
         assertEquals(child, result.scope)
         assertEquals(true, result.isFound)
     }
